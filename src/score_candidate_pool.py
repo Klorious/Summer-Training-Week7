71de4f24151372c64e73200bf63e46e61fb80946
Klorious <aura13a@gmail.com>
2026-07-25 20:02:34 +0800

    Fix ranking scorer module import

--- src/score_candidate_pool.py
+++ src/score_candidate_pool.py
@@ -8,7 +8,7 @@ from PIL import Image
 from tqdm import tqdm
 from torchvision import transforms
 
-from src.models import get_quality_model
+from models import get_quality_model
 
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
